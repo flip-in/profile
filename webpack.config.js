3f193c5c93cc0ca4e38c42f105283459a5bddce9
Will Price <pricew86@gmail.com>
2020-03-26 11:45:01 +0800

    added support for gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   entry: "./src/main.js",
   output: {    
     path: path.resolve(__dirname, "dist"),
+    publicPath: '/',
     filename: "main.js"
   },
   mode: "development",
